10b3d3777ccd045bdab2977a58c1501252f7b477
timrusin <timrusin@gmail.com>
2022-11-13 10:48:20 -0600

    got handlechange and place bet button working

--- src/Game.js
+++ src/Game.js
@@ -5,19 +5,28 @@ const Game = () => {
     const [cash, setCash] = useState(1000)
     const [currentBet, setCurrentBet] = useState(null)
 
+    const handleChange = (e) => {
+        setCurrentBet(e.target.value)
+    }
+
+    const placeBet = () => {
+        setCash(cash - currentBet)
+    }
+
+    console.log(currentBet);
   return (
     <div className='game-page-container fade'>
         <div className='cash'>{ `$ ${cash}` }</div>
             <form className='bet'>
-                <select className='bet-form'>
+                <select onChange={handleChange} className='bet-form'>
                     <option value="10">$10</option>
                     <option value="50">$50</option>
                     <option value="100">$100</option>
                     <option value="200">$200</option>
                     <option value="500">$500</option>
                 </select>
-                <button>PLACE BET</button>
             </form>
+                <button onClick={placeBet}>PLACE BET</button>
     </div>
   )
 }
