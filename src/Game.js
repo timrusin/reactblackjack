a16d8b9c52246ad2c811e4efaa9e1a050651d24c
timrusin <timrusin@gmail.com>
2022-11-13 10:11:42 -0600

    stuck on bet form

--- src/Game.js
+++ src/Game.js
@@ -8,19 +8,16 @@ const Game = () => {
   return (
     <div className='game-page-container fade'>
         <div className='cash'>{ `$ ${cash}` }</div>
-            <div className='bet'>
-                <form>
-                    <select className='bet-form'>
-                        <option value="10">$10</option>
-                        <option value="50">$50</option>
-                        <option value="100">$100</option>
-                        <option value="200">$200</option>
-                        <option value="500">$500</option>
-                    </select>
-                    <button type="submit">PLACE BET</button>
-                </form>
-            </div>
-        
+            <form className='bet'>
+                <select className='bet-form'>
+                    <option value="10">$10</option>
+                    <option value="50">$50</option>
+                    <option value="100">$100</option>
+                    <option value="200">$200</option>
+                    <option value="500">$500</option>
+                </select>
+                <button>PLACE BET</button>
+            </form>
     </div>
   )
 }
