464e1861377593ee1e91fb834b0369ef79e9fcb3
timrusin <timrusin@gmail.com>
2022-11-13 01:07:29 -0600

    random cards working for start page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 
 function App() {
   const [randomOpacity, setRandomOpacity] = useState()
-  const [color, setColor] =useState('')
+  const [values, setValues] = useState('')
+  const [color, setColor] = useState('')
   const [suit, setSuit] = useState('')
+  const [valuesTwo, setValuesTwo] = useState('')
+  const [colorTwo, setColorTwo] = useState('')
+  const [suitTwo, setSuitTwo] = useState('')
 
   useEffect(()=>{
     setInterval(()=>{
@@ -13,12 +17,18 @@ function App() {
   },[])
 
   useEffect(()=>{
+    const value = ['K','Q','A','J','10','9','8','7','6','5','4','3','2']
     const suits = ['fa-heart', 'fa-diamond', 'fa-spade', 'fa-club']
     const colors = ['red', 'black']
+    const randomValue = Math.floor(Math.random()*13)
     const randomColor = Math.floor(Math.random()*2)
     const randomSuit = Math.floor(Math.random()*4)
+    setValues(value[randomValue])
     setColor(colors[randomColor])
     setSuit(suits[randomSuit])
+    setValuesTwo(value[Math.floor(Math.random()*13)])
+    setColorTwo(colors[Math.floor(Math.random()*2)])
+    setSuitTwo(suits[Math.floor(Math.random()*4)])
   },[])
 
   return (
@@ -28,32 +38,32 @@ function App() {
         <div className='cards-container'>
           <div className="cards inner-border card-one ">
             <div className="top-left">
-                <h1 style={{ color: color }} className="D">
-                  D
+                <h1 style={{ color: color }} className="card-value">
+                  {values}
                 </h1> 
               <i style={{ color: color }} className={`fa-solid ${suit}`}></i>
             </div>
             <i style={{ color: color }} className={`fa-solid ${suit} center-suit`}></i>
             <div className="bottom-right">
-                <h1 style={{ color: color }} className="D">
-                  D
+                <h1 style={{ color: color }} className="card-value">
+                  {values}
                 </h1>
               <i style={{ color: color }} className={`fa-solid ${suit}`}></i>
              </div>
           </div>
           <div className="cards inner-border card-two">
             <div className="top-left">
-                <h1 style={{ color: color }} className="D">
-                  D
+                <h1 style={{ color: colorTwo }} className="card-value">
+                  {valuesTwo}
                 </h1> 
-              <i style={{ color: color }} className={`fa-solid ${suit}`}></i>
+              <i style={{ color: colorTwo }} className={`fa-solid ${suitTwo}`}></i>
             </div>
-            <i style={{ color: color }} className={`fa-solid ${suit} center-suit`}></i>
+            <i style={{ color: colorTwo }} className={`fa-solid ${suitTwo} center-suit`}></i>
             <div className="bottom-right">
-                <h1 style={{ color: color }} className="D">
-                  D
+                <h1 style={{ color: colorTwo }} className="card-value">
+                  {valuesTwo}
                 </h1>
-              <i style={{ color: color }} className={`fa-solid ${suit}`}></i>
+              <i style={{ color: colorTwo }} className={`fa-solid ${suitTwo}`}></i>
              </div>
           </div>
         </div>
