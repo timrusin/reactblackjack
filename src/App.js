7e3a139ef305af5ff34cab9ca8d50605c0ba7242
timrusin <timrusin@gmail.com>
2022-11-13 09:19:29 -0600

    cleaned up routing and page management, no more warnings

--- src/App.js
+++ src/App.js
@@ -1,79 +1,14 @@
-import { useEffect, useState } from 'react';
-import { Link, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import Game from './Game';
+import Title from './Title';
 
 function App() {
-  const [randomOpacity, setRandomOpacity] = useState()
-  const [values, setValues] = useState('')
-  const [color, setColor] = useState('')
-  const [suit, setSuit] = useState('')
-  const [valuesTwo, setValuesTwo] = useState('')
-  const [colorTwo, setColorTwo] = useState('')
-  const [suitTwo, setSuitTwo] = useState('')
-
-  useEffect(()=>{
-    setInterval(()=>{
-      setRandomOpacity(Math.random())
-    },300)
-  },[])
-
-  useEffect(()=>{
-    const value = ['K','Q','A','J','10','9','8','7','6','5','4','3','2']
-    const suits = ['fa-heart', 'fa-diamond', 'fa-spade', 'fa-club']
-    const colors = ['red', 'black']
-    const randomValue = Math.floor(Math.random()*13)
-    const randomColor = Math.floor(Math.random()*2)
-    const randomSuit = Math.floor(Math.random()*4)
-    setValues(value[randomValue])
-    setColor(colors[randomColor])
-    setSuit(suits[randomSuit])
-    setValuesTwo(value[Math.floor(Math.random()*13)])
-    setColorTwo(colors[Math.floor(Math.random()*2)])
-    setSuitTwo(suits[Math.floor(Math.random()*4)])
-  },[])
-
+  
   return (
     <div className="App">
-      <h1 className='game-title' >BLACK JACK REACT</h1>
-
-      <Link to='/game'><button className='play-button' style={{opacity: randomOpacity}}>PLAY</button></Link>
-        
-        <div className='cards-container'>
-
-          <div className="cards inner-border card-one ">
-            <div className="top-left">
-                <h1 style={{ color: color }} className="card-value">
-                  {values}
-                </h1> 
-              <i style={{ color: color }} className={`fa-solid ${suit}`}></i>
-            </div>
-            <i style={{ color: color }} className={`fa-solid ${suit} center-suit`}></i>
-            <div className="bottom-right">
-                <h1 style={{ color: color }} className="card-value">
-                  {values}
-                </h1>
-              <i style={{ color: color }} className={`fa-solid ${suit}`}></i>
-             </div>
-          </div>
-
-          <div className="cards inner-border card-two">
-            <div className="top-left">
-                <h1 style={{ color: colorTwo }} className="card-value">
-                  {valuesTwo}
-                </h1> 
-              <i style={{ color: colorTwo }} className={`fa-solid ${suitTwo}`}></i>
-            </div>
-            <i style={{ color: colorTwo }} className={`fa-solid ${suitTwo} center-suit`}></i>
-            <div className="bottom-right">
-                <h1 style={{ color: colorTwo }} className="card-value">
-                  {valuesTwo}
-                </h1>
-              <i style={{ color: colorTwo }} className={`fa-solid ${suitTwo}`}></i>
-             </div>
-          </div>
-        </div>
         <Routes>
+          <Route path='/' element={<Title/>} />
           <Route path="/game" element={<Game/>} />
         </Routes>
     </div>
