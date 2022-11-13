6c1af5815eaebf7a91c334e3892f4108008da7e4
timrusin <timrusin@gmail.com>
2022-11-13 10:02:23 -0600

    trying to figure out state with bet form

--- src/Game.js
+++ src/Game.js
@@ -3,11 +3,24 @@ import './Game.css'
 
 const Game = () => {
     const [cash, setCash] = useState(1000)
+    const [currentBet, setCurrentBet] = useState(null)
+
   return (
     <div className='game-page-container fade'>
         <div className='cash'>{ `$ ${cash}` }</div>
-    
-    
+            <div className='bet'>
+                <form>
+                    <select className='bet-form'>
+                        <option value="10">$10</option>
+                        <option value="50">$50</option>
+                        <option value="100">$100</option>
+                        <option value="200">$200</option>
+                        <option value="500">$500</option>
+                    </select>
+                    <button type="submit">PLACE BET</button>
+                </form>
+            </div>
+        
     </div>
   )
 }
