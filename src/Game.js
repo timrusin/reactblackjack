3694f5d2a565053c5e239acd5ced36ac2370eed2
timrusin <timrusin@gmail.com>
2022-11-13 09:26:11 -0600

    added state for cash and cash element in place starting at 1000

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
 import './Game.css'
 
 const Game = () => {
+    const [cash, setCash] = useState(1000)
   return (
-    <div className='game-page-container fade'>Game</div>
+    <div className='game-page-container fade'>
+        <div className='cash'>{ `$ ${cash}` }</div>
+    
+    
+    </div>
   )
 }
 
