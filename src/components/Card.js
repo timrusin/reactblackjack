50669e9559e2af5b2f01f73be687910a918e8c08
timrusin <timrusin@gmail.com>
2022-11-14 20:17:39 -0600

    moved bottom right up a touch

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,9 +9,9 @@ const Card = (props) => {
           {props.char}
         </h1> 
       <i style={{ color: props.color }} className={`fa-solid ${props.suit}`}></i>
-    </div>
-    <i style={{ color: props.color }} className={`fa-solid ${props.suit} center-suit`}></i>
-    <div className="bottom-right">
+      </div>
+      <i style={{ color: props.color }} className={`fa-solid ${props.suit} center-suit`}></i>
+      <div className="bottom-right">
         <h1 style={{ color: props.color }} className="card-value">
           {props.char}
         </h1>
